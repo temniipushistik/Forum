cb947e806e1922a1fd85067a27b0df965a026cbc
Gennadii Koshelev <gennadii.koshelev@mera.com>
2022-01-19 16:32:02 +0300

    add correct link to reg

--- src/main/java/com/example/forumpilotproject/entities/Message.java
+++ src/main/java/com/example/forumpilotproject/entities/Message.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Getter
@@ -21,6 +18,8 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String text;
+    @ManyToOne
+    private EntityUser author;
 
     public Message(String text) {
         this.text = text;
